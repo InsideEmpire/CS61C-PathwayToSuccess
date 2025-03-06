9fcab479e70cb5e165abd8356cc14b875b4e2a95
Garrison <garrison_liu@outlook.com>
2025-03-06 07:53:10 +0000

    finish Exercise 2: Linear Feedback Shift Register

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -5,6 +5,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    *reg = (*reg >> 1) | ((((*reg >> 5) ^ (*reg >> 3) ^ (*reg >> 2) ^ (*reg >> 0)) & 1) << 15);
 }
 
