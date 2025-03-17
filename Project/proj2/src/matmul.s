8e9082520db9230814f608f5cc79ba444f6d6800
Garrison <garrison_liu@outlook.com>
2025-03-17 20:17:32 +0800

    finish Proj2 Part A: Mathematical Functions (#5)
    * update venus
    
    * finish Task 0: Getting Started with the Abs Function
    
    * finish Task 1: ReLU
    
    * finish Task 2: ArgMax
    
    * finish Task 3.1: Dot Product
    
    * finish Task 3.2: Matrix Multiplication

--- Project/proj2/src/matmul.s
+++ Project/proj2/src/matmul.s
@@ -24,40 +24,94 @@
 #     this function terminates the program with exit code 74.
 # =======================================================
 matmul:
-
-    # Error checks
-
-
+    addi sp, sp, -28
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw s4, 16(sp)
+    sw s5, 20(sp)
+    sw ra, 24(sp)
     # Prologue
 
-
+    # ======================================
+    mv s0, a0 # m0
+    mv s1, a3 # m1
+    mv s3, a6 # d
+    mv s4, a1 # height of m0 and width of m1
+    mv s5, a2 # width of m0 and height of m0
+    # ======================================
+    
+    li t1, 0 # i = 0 outer m0's ith row
+    li t2, 0 # j = 0 inner m1's jth column
 outer_loop_start:
-
-
-
-
+    li t2, 0
 inner_loop_start:
+    addi sp, sp, -28
+    sw a0, 0(sp)
+    sw a1, 4(sp)
+    sw a2, 8(sp)
+    sw a3, 12(sp)
+    sw a4, 16(sp)
+    sw t1, 20(sp)
+    sw t2, 24(sp)
+    
+    # Calculate row index for m0
+    slli t6, t1, 2        
+    mul t6, t6, s5        # t6 = i * m0_width * 4
+    add a0, s0, t6        # a0 = m0(s0) + t6 (m0's row start)
 
+    # Calculate column index for m1
+    slli t5, t2, 2        # t5 = j * 4 (column offset for m1)
+    add a1, s1, t5        # a1 = m1(s1) + t5 (m1's column start)
 
+    # Set other arguments for dot product
+    mv a2, s5             # length (width of m0 and height of m1)
+    li a3, 1
+    mv a4, s4
+    # FUNCTION: Dot product of 2 int vectors
+    # Arguments:
+    #   a0 (int*) is the pointer to the start of v0
+    #   a1 (int*) is the pointer to the start of v1
+    #   a2 (int)  is the length of the vectors
+    #   a3 (int)  is the stride of v0
+    #   a4 (int)  is the stride of v1
+    # Returns:
+    #   a0 (int)  is the dot product of v0 and v1
+    jal ra, dot            # Call dot
 
-
-
-
-
-
-
-
-
-
+    # Restore the stack and store result in d
+    sw a0, 0(s3)
+    addi s3, s3, 4
+    
+    # Restore registers
+    lw a0, 0(sp)
+    lw a1, 4(sp)
+    lw a2, 8(sp)
+    lw a3, 12(sp)
+    lw a4, 16(sp)
+    lw t1, 20(sp)
+    lw t2, 24(sp)
+    addi sp, sp, 28
+    
 inner_loop_end:
-
-
-
+    addi t2, t2, 1        # increment column index
+    bge t2, s4, outer_loop_end
+    j inner_loop_start
 
 outer_loop_end:
+    addi t1, t1, 1        # increment row index
+    bge t1, s4, done      # check if done
+    j outer_loop_start
 
-
+done:   
     # Epilogue
-    
-    
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw s4, 16(sp)
+    lw s5, 20(sp)
+    lw ra, 24(sp)
+    addi sp, sp, 28
     ret
--- Project/proj2/unittests/unittests.py
+++ Project/proj2/unittests/unittests.py
@@ -22,6 +22,13 @@ class TestAbs(TestCase):
         t.check_scalar("a0", 1)
         t.execute()
 
+    def test_minus_one(self):
+        t = AssemblyTest(self, "abs.s")
+        t.input_scalar("a0", -1)
+        t.call("abs")
+        t.check_scalar("a0", 1)
+        t.execute()
+
     @classmethod
     def tearDownClass(cls):
         print_coverage("abs.s", verbose=False)
@@ -43,6 +50,21 @@ class TestRelu(TestCase):
         # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
         t.execute()
 
+    def test_zero(self):
+        t = AssemblyTest(self, "relu.s")
+        # create an array in the data section
+        array0 = t.array([-1, -2, -3, -4, -5, -6, -7, -8, -9])
+        # load address of `array0` into register a0
+        t.input_array("a0", array0)
+        # set a1 to the length of our array
+        t.input_scalar("a1", len(array0))
+        # call the relu function
+        t.call("relu")
+        # check that the array0 was changed appropriately
+        t.check_array(array0, [0, 0, 0, 0, 0, 0, 0, 0, 0])
+        # generate the `assembly/TestRelu_test_simple.s` file and run it through venus
+        t.execute()
+
     @classmethod
     def tearDownClass(cls):
         print_coverage("relu.s", verbose=False)
@@ -52,16 +74,45 @@ class TestArgmax(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "argmax.s")
         # create an array in the data section
-        raise NotImplementedError("TODO")
-        # TODO
+        array = t.array([4, 2, 3, 4])
         # load address of the array into register a0
-        # TODO
+        t.input_array("a0", array)
         # set a1 to the length of the array
-        # TODO
+        t.input_scalar("a1", 4)
         # call the `argmax` function
-        # TODO
+        t.call("argmax")
         # check that the register a0 contains the correct output
-        # TODO
+        t.check_scalar("a0", 0)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+
+    def test_zero(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array = t.array([0, 0, 0, 0])
+        # load address of the array into register a0
+        t.input_array("a0", array)
+        # set a1 to the length of the array
+        t.input_scalar("a1", 4)
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 0)
+        # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
+        t.execute()
+
+    def test_negative(self):
+        t = AssemblyTest(self, "argmax.s")
+        # create an array in the data section
+        array = t.array([-1, 0, 2])
+        # load address of the array into register a0
+        t.input_array("a0", array)
+        # set a1 to the length of the array
+        t.input_scalar("a1", 3)
+        # call the `argmax` function
+        t.call("argmax")
+        # check that the register a0 contains the correct output
+        t.check_scalar("a0", 2)
         # generate the `assembly/TestArgmax_test_simple.s` file and run it through venus
         t.execute()
 
@@ -74,16 +125,37 @@ class TestDot(TestCase):
     def test_simple(self):
         t = AssemblyTest(self, "dot.s")
         # create arrays in the data section
-        raise NotImplementedError("TODO")
-        # TODO
+        array0 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        array1 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
         # load array addresses into argument registers
-        # TODO
+        t.input_array("a0", array0)
+        t.input_array("a1", array1)
         # load array attributes into argument registers
-        # TODO
+        t.input_scalar("a2", 9)
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 1)
         # call the `dot` function
         t.call("dot")
         # check the return value
-        # TODO
+        t.check_scalar("a0", 285)
+        t.execute()
+
+    def test_stride(self):
+        t = AssemblyTest(self, "dot.s")
+        # create arrays in the data section
+        array0 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        array1 = t.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        # load array addresses into argument registers
+        t.input_array("a0", array0)
+        t.input_array("a1", array1)
+        # load array attributes into argument registers
+        t.input_scalar("a2", 3)
+        t.input_scalar("a3", 1)
+        t.input_scalar("a4", 2)
+        # call the `dot` function
+        t.call("dot")
+        # check the return value
+        t.check_scalar("a0", 22)
         t.execute()
 
     @classmethod
@@ -104,16 +176,20 @@ class TestMatmul(TestCase):
         array_out = t.array([0] * len(result))
 
         # load address of input matrices and set their dimensions
-        raise NotImplementedError("TODO")
-        # TODO
+        t.input_array("a0", array0)
+        t.input_scalar("a1", m0_rows)
+        t.input_scalar("a2", m0_cols)
+        t.input_array("a3", array1)
+        t.input_scalar("a4", m1_rows)
+        t.input_scalar("a5", m1_cols)
         # load address of output array
-        # TODO
+        t.input_array("a6", array_out)
 
         # call the matmul function
         t.call("matmul")
 
         # check the content of the output array
-        # TODO
+        t.check_array(array_out, result)
 
         # generate the assembly file and run it through venus, we expect the simulation to exit with code `code`
         t.execute(code=code)
@@ -123,6 +199,27 @@ class TestMatmul(TestCase):
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [1, 2, 3, 4, 5, 6, 7, 8, 9], 3, 3,
             [30, 36, 42, 66, 81, 96, 102, 126, 150]
+        )
+
+    def test_2x3Dot3x2(self):
+        self.do_matmul(
+            [1, 0, 2, -1, 3, 1], 2, 3,
+            [3, 1, 2, 1, 1, 0], 3, 2,
+            [5, 1, 4, 2]
+        )
+
+    def test_3x2Dot2x3(self):
+        self.do_matmul(
+            [1, 0, 2, -1, 3, 1], 3, 2,
+            [3, 1, 2, 1, 1, 0], 2, 3,
+            [3, 1, 2, 5, 1, 4, 10, 4, 6]
+        )
+
+    def test_zero(self):
+        self.do_matmul(
+            [0, 0], 1, 2,
+            [0, 0], 2, 1,
+            [0]
         )
 
     @classmethod
