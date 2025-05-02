806df448b0ac4c9356d0c92a442e85f38adbf1d5
InsideEmpire <garrison_liu@outlook.com>
2025-05-02 21:49:21 +0800

    finish Exercise 3 - Dot Product

--- Lab/lab10/omp_apps.c
+++ Lab/lab10/omp_apps.c
@@ -85,10 +85,11 @@ double dotp_manual_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
   #pragma omp parallel
   {
+    double single_sum = 0.0;
     #pragma omp for
     for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
-      global_sum += x[i] * y[i];
+      single_sum += x[i] * y[i];
+    global_sum += single_sum;
   }
   return global_sum;
 }
@@ -100,9 +101,8 @@ double dotp_reduction_optimized(double* x, double* y, int arr_size) {
   double global_sum = 0.0;
   #pragma omp parallel
   {
-    #pragma omp for
+    #pragma omp for reduction(+:global_sum)
     for (int i = 0; i < arr_size; i++)
-      #pragma omp critical
       global_sum += x[i] * y[i];
   }
   return global_sum;
